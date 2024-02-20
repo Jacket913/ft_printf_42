751e5788573f1a9d78d8b948218448be95b4b322
Jacket913 <>
2024-02-20 10:02:52 +0100

    Push before correction

--- ft_printf.c
+++ ft_printf.c
@@ -6,30 +6,30 @@
 /*   By: gmoulin <gmoulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/12 17:56:28 by gmoulin           #+#    #+#             */
-/*   Updated: 2024/01/25 14:25:25 by gmoulin          ###   ########.fr       */
+/*   Updated: 2024/01/26 12:39:46 by gmoulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	check_format(char c, va_list args)
+int	check_format(char c, va_list *args)
 {
 	if (c == 'c')
-		return (ft_putchar(va_arg(args, int)));
+		return (ft_putchar(va_arg(args[0], int)));
 	else if (c == 's')
-		return (ft_putstr(va_arg(args, char *)));
+		return (ft_putstr(va_arg(args[0], char *)));
 	else if (c == 'p')
-		return (ft_print_ptr(va_arg(args, void *)));
+		return (ft_print_ptr(va_arg(args[0], void *)));
 	else if (c == 'd')
-		return (pnbase(va_arg(args, int), 10, "0123456789"));
+		return (pnbase(va_arg(args[0], int), 10, "0123456789"));
 	else if (c == 'i')
-		return (pnbase(va_arg(args, int), 10, "0123456789"));
+		return (pnbase(va_arg(args[0], int), 10, "0123456789"));
 	else if (c == 'u')
-		return (pnbase(va_arg(args, unsigned int), 10, "0123456789"));
+		return (pnbase(va_arg(args[0], unsigned int), 10, "0123456789"));
 	else if (c == 'x')
-		return (pnbase(va_arg(args, unsigned int), 16, "0123456789abcdef"));
+		return (pnbase(va_arg(args[0], unsigned int), 16, "0123456789abcdef"));
 	else if (c == 'X')
-		return (pnbase(va_arg(args, unsigned int), 16, "0123456789ABCDEF"));
+		return (pnbase(va_arg(args[0], unsigned int), 16, "0123456789ABCDEF"));
 	else if (c == '%')
 		return (ft_putchar('%'));
 	return (0);
@@ -50,7 +50,7 @@ int	ft_printf(const char *format, ...)
 	while(format[i] != '\0')
 	{
 		if (format[i] == '%')
-			length += check_format(format[++i], args);
+			length += check_format(format[++i], &args);
 		else
 			length += ft_putchar(format[i]);
 		i++;
@@ -58,3 +58,7 @@ int	ft_printf(const char *format, ...)
 	va_end(args);
 	return (length);
 }
+
+/*int main() {
+	ft_printf("yo %c, %c\n", '0', 0);
+}*/
